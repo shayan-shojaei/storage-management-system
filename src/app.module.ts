1dc67a301bdf0ffaecdaa251d56e6597ed5b89e7
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2022-09-11 10:58:01 +0430

    feat: setup mongo database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongoModule } from 'nest-mongodb';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongoModule.forRoot(process.env.MONGO_URI, process.env.MONGO_DB),
+  ],
   controllers: [],
   providers: [],
 })
